878718ad85891330ea5c661f68015b421234a365
Pavan Chilukuri <nh111392@hertz.net>
2022-09-28 08:46:34 -0500

    Add routes and api endpoints

---
+++ models/model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Data", dataSchema);
